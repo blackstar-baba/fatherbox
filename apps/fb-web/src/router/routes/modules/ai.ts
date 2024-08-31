5a2f102da0539912379b120fd7ddfb15bbffe0ce
blackstar-baba <535650957@qq.com>
2024-08-31 22:44:59 +0800

    feat: support remote llms (#10)

---
+++ apps/fb-web/src/router/routes/modules/ai.ts
@@ -0,0 +1,31 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+import { BasicLayout } from '#/layouts';
+import { $t } from '#/locales';
+
+const routes: RouteRecordRaw[] = [
+  {
+    component: BasicLayout,
+    meta: {
+      icon: 'vscode-icons:file-type-ai',
+      order: 30,
+      title: $t('page.ai.title'),
+    },
+    name: 'AI',
+    path: '/',
+    children: [
+      {
+        name: 'Remote',
+        path: '/remote',
+        component: () => import('#/views/ai/remote/index.vue'),
+        meta: {
+          affixTab: true,
+          icon: 'carbon:ai-launch',
+          title: $t('page.ai.remote'),
+        },
+      },
+    ],
+  },
+];
+
+export default routes;
